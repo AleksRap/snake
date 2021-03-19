7267dbeb0ea644d51e4b8d283dd3369e0452f4a3
Седов Алексей <aleks.sedov@mail.ru>
2021-03-19 23:11:28 +0300

    updated ui reducer

--- src/store/ui/reducer.ts
+++ src/store/ui/reducer.ts
@@ -1,7 +1,8 @@
 import { UIState, RequestStatus } from 'types';
+import { MeActionTypes } from 'store/me/actionTypes';
 
 const initialState: UIState = {
-
+  [MeActionTypes.CHANGE_THEME_SUCCESS]: RequestStatus.INIT,
 };
 
 export default (state: UIState = initialState, action: any): UIState => {
