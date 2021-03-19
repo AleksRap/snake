887a07dd54be1fc4a598d8f785e92ca688d7f92c
Седов Алексей <aleks.sedov@mail.ru>
2021-03-20 01:14:59 +0300

    added type container gameTable

---
+++ src/types/containers/gameTable.ts
@@ -0,0 +1,13 @@
+export enum KeyCodes {
+  ArrowUp = 'ArrowUp',
+  ArrowDown = 'ArrowDown',
+  ArrowLeft = 'ArrowLeft',
+  ArrowRight = 'ArrowRight',
+}
+
+export enum Direction {
+  up = 'up',
+  down = 'down',
+  left = 'left',
+  right = 'right',
+}
