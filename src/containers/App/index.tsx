4b2a1b105bc800eaee62bfced080f59e062aa493
Седов Алексей <aleks.sedov@mail.ru>
2021-03-19 23:12:25 +0300

    fixed App container

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { FC, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import meSelectors from 'store/me/selectors';
+import { Theme } from 'types';
+import styles from './styles.module.scss';
 
-const App = () => (
-  <>
+const App: FC = () => {
+  const theme = useSelector(meSelectors.getProp('theme'));
 
-  </>
-);
+  useEffect(() => {
+    const body = document.querySelector('body');
+    if (body) {
+      if (theme === Theme.light) {
+        body.classList.add(theme);
+        body.classList.remove(Theme.dark);
+      } else {
+        body.classList.add(theme);
+        body.classList.remove(Theme.light);
+      }
+    }
+  }, [theme]);
+
+  return (
+    <div className={styles.background} />
+  );
+};
 
 export default App;
